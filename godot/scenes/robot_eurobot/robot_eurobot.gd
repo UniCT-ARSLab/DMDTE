825dd8157d6286ea66b15c41cb0c9d3b7ee359f4
fedyfausto <fedyfausto@hotmail.com>
2025-12-30 13:16:01 +0100

    removed pose from normal node of eurobot robot

--- godot/scenes/robot_eurobot/robot_eurobot.gd
+++ godot/scenes/robot_eurobot/robot_eurobot.gd
@@ -1,8 +1,1 @@
 class_name RobotEurobot extends DigitalTwin
-
-var pose : Vector3
-
-func _on_dtp_peer_on_update(dtpm: DTPPeer, update: Dictionary) -> void:
-	for key in update:
-		if key in self:
-			self[key] = update[key]
